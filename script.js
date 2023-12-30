f1eb4e282099066040bda450524c8f222feef014
michaeltaman <42732094+michaeltaman@users.noreply.github.com>
2023-12-30 17:17:37 +0200

    confetti + audio addedt

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let dragStopPositions = []; // Array to store drag stop positions
+
 document.querySelectorAll('div img').forEach(el => {
   let isDragging = false;
 
@@ -28,10 +30,31 @@ document.querySelectorAll('div img').forEach(el => {
     document.addEventListener('touchmove', moveItem);
   }
 
-  function stopDragging() {
+  function stopDragging(e) {
     isDragging = false;
     document.removeEventListener('mousemove', moveItem);
     document.removeEventListener('touchmove', moveItem);
+
+    // Save the drag stop position
+    dragStopPositions.push({ x: e.pageX, y: e.pageY });
+
+    // Check if there are 5 saved positions
+    if (dragStopPositions.length === 5) {
+      launchConfettiAnimation();
+    }
+  }
+
+  //var audio = new Audio('https://upload.wikimedia.org/wikipedia/commons/3/34/Sound_Effect_-_Door_Bell.ogg');
+  var audio = new Audio('./enchanted-chimes-177906.mp3');
+
+  function launchConfettiAnimation() {
+    // Your code to launch confetti animation goes here
+    // This function will be called when 5 drag stop positions are saved
+    console.log('Launching confetti animation!');
+    toggleConfetti();
+    audio.play();
+    // Replace console.log with your confetti animation code
+    // For example, you can use libraries like confetti-js to create confetti animation
   }
 
   el.addEventListener('mouseup', stopDragging);
