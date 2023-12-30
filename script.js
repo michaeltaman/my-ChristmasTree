76d65ff868f1534fc36a831018ab44c9aa53c9b9
michaeltaman <42732094+michaeltaman@users.noreply.github.com>
2023-12-30 12:51:01 +0200

    added touchedmove

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 document.querySelectorAll('div img').forEach(el => {
-  el.onmousedown = e => {
+  let isDragging = false;
+
+  el.addEventListener('mousedown', startDragging);
+  el.addEventListener('touchstart', startDragging);
+
+  function startDragging(e) {
+    e.preventDefault();
+
+    isDragging = true;
     document.body.append(el);
     el.style.position = 'absolute';
     el.ondragstart = () => false;
 
     moveItem(e);
+
     function moveItem(e) {
-      el.style.top = e.pageY - el.height / 2 + 'px';
-      el.style.left = e.pageX - el.width / 2 + 'px';
+      if (isDragging) {
+        if (e.type === 'touchmove') {
+          e = e.touches[0];
+        }
+        el.style.top = e.pageY - el.height / 2 + 'px';
+        el.style.left = e.pageX - el.width / 2 + 'px';
+      }
     }
 
-    document.onmousemove = e => moveItem(e);
-    el.onmouseup = () => document.onmousemove = '';
-  };
+    document.addEventListener('mousemove', moveItem);
+    document.addEventListener('touchmove', moveItem);
+
+    el.addEventListener('mouseup', stopDragging);
+    el.addEventListener('touchend', stopDragging);
+  }
+
+  function stopDragging() {
+    isDragging = false;
+    document.removeEventListener('mousemove', moveItem);
+    document.removeEventListener('touchmove', moveItem);
+  }
 });
